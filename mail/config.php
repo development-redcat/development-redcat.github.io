a8a86ae0e4306488100e2a33a090916cd7ab76f8
development-redcat <64353431+development-redcat@users.noreply.github.com>
2020-07-09 13:19:52 +0200

    upadate fancybox 2

---
+++ mail/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('email',          'email@gmail.com');
+
+    define('company_name',   'УБИС');
+    define('recipient_email','email@gmail.com');
+    define('subject',        'Заявка с сайта');
+    define('auto_responder_subject', 'Autorepsonse: Мы получили ваше сообщение');
